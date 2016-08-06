0d629a8c712634dc57c5b894b52a409c9924993a
Luis Fernando Forero G. <foluis@hotmail.com>
2016-08-06 15:19:25 -0500

    Create Parkinglot Form

---
+++ Estacionamientos/EstacionamientoDF/ViewModels/ParkingLotViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EstacionamientoDF.ViewModels
+{
+    public class ParkingLotViewModel
+    {
+        [Required]
+        [StringLength(200)]
+        public string Name { get; set; }
+
+        [Required]
+        public decimal Latitude { get; set; }
+
+        [Required]
+        public decimal Longitude { get; set; }
+    }
+}
